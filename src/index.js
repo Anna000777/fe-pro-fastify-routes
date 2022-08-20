bec12fffecc8959c5626425892bb562204a121fb
Anna <kazanok777@gmail.com>
2022-08-20 16:49:41 +0300

    added fastify routes

--- src/index.js
+++ src/index.js
@@ -11,4 +11,40 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request,reply) => {
+
+  if(request.body.toLowerCase().includes('fuck')){
+    return reply.status(403).send('unresolved');
+  }
+  reply.send(request.body.toUpperCase());
+
+})
+
+fastify.post('/lowercase', (request,reply) => {
+
+  if(request.body.toLowerCase().includes('fuck')){
+    return reply.status(403).send('unresolved');
+  }
+  reply.send(request.body.toLowerCase());
+
+})
+
+fastify.get('/user/:id', (request, reply) => {
+  const {id} = request.params;
+  if (users[id]){
+    return reply.send(users[id]);
+  }
+  reply.status(400).send('User not exist');
+})
+
+fastify.get('/users', (request, reply) => {
+  const {filter,value} = request.query;
+  let result = Object.values(users);
+  if (filter && value) {
+    return result.filter((user) => user[filter] === value);
+  }
+
+  reply.send(result);
+})
+
 export default fastify;
